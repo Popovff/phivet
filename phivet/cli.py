a70185b5011995ccd3542f385c0729a63f72deee
Popovff <Popovff@users.noreply.github.com>
2026-02-20 12:46:06 +0000

    ✅ Ready to clone and code.

--- phivet/cli.py
+++ phivet/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for phivet project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m phivet` and `$ phivet `.
 
     This is your program's entry point.
 
